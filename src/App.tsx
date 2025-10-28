642d4f397c8d0148f48174d6166b844d4a0649d6
Shailesh <126746445+shaileshkvr@users.noreply.github.com>
2025-10-28 20:56:43 +0530

    Refactor App component to replace multiple UseStateCard instances with UseEffectCard

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import grid from "./assets/images/grid.png";
+import UseEffectCard from "./components/UseEffectCard/UseEffectCard.js";
 import UseStateCard from "./components/UseStateCard/UseStateCard.js";
 
 const App = () => {
@@ -10,11 +11,7 @@ const App = () => {
       <h1 className="text-4xl text-gray-200 p-5">React Hooks</h1>
       <div className={`flex flex-wrap gap-8 px-5`}>
         <UseStateCard />
-        <UseStateCard />
-        <UseStateCard />
-        <UseStateCard />
-        <UseStateCard />
-        <UseStateCard />
+        <UseEffectCard />
       </div>
     </div>
   );
